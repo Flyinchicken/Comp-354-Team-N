eb6811c613fdd4942390efd4d59b32ef6ab116d1
gabrieltalih <78610215+gabrieltalih@users.noreply.github.com>
2023-04-01 01:06:17 -0400

    Update hyperbolicSine.js

--- src/js/functions/hyperbolicSine.js
+++ src/js/functions/hyperbolicSine.js
@@ -1,9 +1,6 @@
 const naturalExponentiation = require("./naturalExponentiation.js");
 
-function hyperbolicSine(x) {
-    // check for invalid input
-    if(typeof x != 'number') return NaN;
-    
+function hyperbolicSine(x) {    
     let result = (naturalExponentiation(x) - naturalExponentiation(-x))/2;
     
     return parseFloat(result.toPrecision(10));
