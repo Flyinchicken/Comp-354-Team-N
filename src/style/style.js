add61065ddc0d86c61e99acbf0c7649ab9078f20
Devansh <99159017+Devansh-Vaidya@users.noreply.github.com>
2023-04-01 20:34:07 -0400

    Closes UI improvements and Eternity heading has infinte line effect

--- src/style/style.js
+++ src/style/style.js
@@ -1,3 +1,7 @@
+$(document).ready(function () {
+    $("#change-theme").click(changeTheme);
+});
+
 function changeTheme() {
     const elements = document.querySelectorAll('*');
     const excludedElements = document.getElementsByName('header');
@@ -7,11 +11,8 @@ function changeTheme() {
         if (element.getAttribute("data-bs-theme") === "dark") {
             element.setAttribute("data-bs-theme", "light"); // Set to light mode
         } else {
-            if (element.id.includes("Btn")) {
+            if (element.id.includes("Btn") || element.id.includes("button-addon2")) {
                 element.classList.toggle("btn-outline-dark");
-                element.classList.toggle("btn-outline-light");
-            } else if (element.id.includes("button-addon2")) {
-                element.classList.toggle("btn-outline-secondary");
                 element.classList.toggle("btn-outline-light");
             } else {
                 element.setAttribute("data-bs-theme", "dark"); // Set to dark mode
@@ -23,6 +24,8 @@ function changeTheme() {
     excludedElements.forEach(element => {
         element.setAttribute("data-bs-theme", "light");
         element.classList.toggle("btn-light");
+        element.classList.toggle("bg-body-tertiary");
+        element.classList.toggle("bg-dark-subtle");
     });
 
     // Change theme icon
