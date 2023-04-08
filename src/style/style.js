2db8de798ef886841e2c3fc92fc605a105f0204c
Devansh <99159017+Devansh-Vaidya@users.noreply.github.com>
2023-04-08 19:38:47 -0400

    Closes Issue#14 : Added Change Precision input box, other UI improvements

--- src/style/style.js
+++ src/style/style.js
@@ -2,11 +2,11 @@ $(document).ready(function () {
     $("#change-theme").click(changeTheme);
 });
 
+const excludedElements = document.getElementsByClassName("header");
+
 function changeTheme() {
     const elements = document.querySelectorAll('*');
-    const excludedElements = document.getElementsByName('header');
     const themeIcon = document.getElementById('theme-icon');
-
     elements.forEach(element => {
         if (element.getAttribute("data-bs-theme") === "dark") {
             element.setAttribute("data-bs-theme", "light"); // Set to light mode
@@ -14,19 +14,21 @@ function changeTheme() {
             if (element.id.includes("Btn") || element.id.includes("button-addon2")) {
                 element.classList.toggle("btn-outline-dark");
                 element.classList.toggle("btn-outline-light");
-            } else {
+            } else if (element.id.includes("navbar-setting")) {
+                element.classList.toggle("btn-light");
+            }
+            else {
                 element.setAttribute("data-bs-theme", "dark"); // Set to dark mode
             }
         }
     });
 
-    // Set excluded elements (navbar elements) to light mode
-    excludedElements.forEach(element => {
-        element.setAttribute("data-bs-theme", "light");
-        element.classList.toggle("btn-light");
-        element.classList.toggle("bg-body-tertiary");
-        element.classList.toggle("bg-dark-subtle");
-    });
+    // console.log(excludedElements);
+
+    for (let i = 0; i < excludedElements.length; i++) {
+        excludedElements[i].setAttribute("data-bs-theme", "light");
+        excludedElements[i].classList.toggle("bg-dark-subtle");
+    }
 
     // Change theme icon
     themeIcon.classList.toggle("bi-sun");
